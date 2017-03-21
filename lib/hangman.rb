e1bf87765e6b834e5c815272636a88ebd5e46413
Oleh Sliusar <oleh@olehsliusar.com>
2017-03-21 09:16:20 +0200

    Define select_word method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,16 @@
 class Hangman
+  def initialize
+    puts "Welcome to Hangman!"
+    puts select_word
+  end
 
+  def select_word
+    File.open("5desk.txt") do |file|
+      words = file.readlines
+      selected_words = words.select { |word| word.length.between?(5, 12) }
+      random_word = selected_words.sample.strip
+    end
+  end
 end
+
+new_game = Hangman.new
